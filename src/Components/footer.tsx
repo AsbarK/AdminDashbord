52fa701b3ae3ff4d757bdfbe96ddc9affa10870c
asbar <asbar03KK@gmail.com>
2023-07-19 22:42:49 +0530

    Changed footer

--- src/Components/footer.tsx
+++ src/Components/footer.tsx
@@ -1,23 +1,34 @@
 function Footer() {
   return (
     <>
-      <div className="flex justify-between border-2 border-main-bg border-t-soft-bg">
-        <div className="flex">
-          <div className="text-soft-color text-lg font-bold mt-2 p-2">
-            Author:
-          </div>
-          <div className="text-main-color text-xl font-bold mt-2 p-2">
-            K Mohammed Asbar
-          </div>
+      <div className="flex flex-col border-2 border-main-bg border-t-soft-bg justify-center items-center">
+        <div className="text-main-color text-xl font-bold mt-2 p-2">
+          <span className="m-2">Hi, This is</span>K Mohammed Asbar
         </div>
-
-        <a href="https://linkedin.com/in/asbar-k-786asbar">
-          <img
-            src="linkdin.png"
-            alt="linkdin logo"
-            className="w-10 rounded-xl mt-2"
-          />
-        </a>
+        <div className="flex gap-2 items-center">
+          <span className="m-2 text-main-color text-xl">Connect:</span>
+          <a href="https://linkedin.com/in/asbar-k-786asbar">
+            <img
+              src="linkedin.svg"
+              alt="linkdin logo"
+              className="w-10 rounded-xl mt-2"
+            />
+          </a>
+          <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSPFdGzlwnbWXXNGldNLZXsBRrlgVCMrrMWWsMkqwfQdqRVJQBkZgtBclPVPDFRtPvnbmhpj">
+            <img
+              src="gmail.svg"
+              alt="gmail logo"
+              className="w-10 rounded-xl mt-2"
+            />
+          </a>
+          <a href="https://github.com/AsbarK/">
+            <img
+              src="github.svg"
+              alt="github logo"
+              className="w-10 rounded-xl mt-2"
+            />
+          </a>
+        </div>
       </div>
     </>
   );
